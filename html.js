c8a3210a048fe6e66d2e714361cb16efa5d59b52
goodnightng0 <cerisehill@korea.ac.kr>
2020-04-28 00:03:46 +0900

    Yoogle first commit

--- html.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("keyword").addEventListener("change", function () {
-  if (document.getElementById("keyword") > 1) {
-    document.getElementById("input-x").style.visibility = "visible";
-  } else {
-    document.getElementById("input-x").style.visibility = "hidden";
-  }
-});
